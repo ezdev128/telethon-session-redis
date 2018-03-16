17395caa831d5d8696526e4e7aa6e2a403fab748
Konstantin M <ezdev128@yandex.com>
2018-03-16 04:44:03 -0700

    Version increase

--- teleredis/__init__.py
+++ teleredis/__init__.py
@@ -1,7 +1,7 @@
 
 from .teleredis import RedisSession, PackFunction, UnpackFunction
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __author__ = "Konstantin M. <ezdev128@yandex.com>"
 __all__ = [
     "RedisSession",
